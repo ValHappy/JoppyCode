fecfb05a18a65dd7d7dea97fab7dca07d125db09
Valentina Alegría <val.happy.m@gmail.com>
2021-02-28 17:36:21 -0500

    colores e instalación

---
+++ src/config/colors.js
@@ -0,0 +1,13 @@
+export default {
+    black: '#000000',
+    gray: '#424242',
+    white: '#FFFFFF',
+    acua: '#0EBDCA',
+    green: '#0EE788',
+    blue: '#1F8DED',
+    violet: '#5457C1',
+    lila: '#BCBFF2',
+    pink: '#F94673',
+    colortext: '#808F9F',
+    background: '#F4F8FB',
+}
