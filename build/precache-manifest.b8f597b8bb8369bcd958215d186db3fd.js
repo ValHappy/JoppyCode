43409f8edc0d211014a6e63a5caee2e055a16c40
deirk <deirkh@gmail.com>
2021-04-17 18:16:25 -0500

    Deploy por subdia a github corregido

---
+++ build/precache-manifest.b8f597b8bb8369bcd958215d186db3fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d562998283d0ae7cb2803d32bcc21c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ffaac8d2513863b1099",
+    "url": "/static/js/2.8ed51155.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/static/js/2.8ed51155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001af0efd226abec303e",
+    "url": "/static/js/main.8419d8c7.chunk.js"
+  },
+  {
+    "revision": "a6613213d83930de28b4",
+    "url": "/static/js/runtime-main.da1778af.js"
+  },
+  {
+    "revision": "716387fc451dfc15c10100a96f1d9376",
+    "url": "/static/media/Joppy.716387fc.png"
+  },
+  {
+    "revision": "8f9ad984bb3379d4a56481fbb6a0c6d8",
+    "url": "/static/media/JoppyCode.8f9ad984.svg"
+  },
+  {
+    "revision": "afd16a2708457d7fb1bbd8b6d4e38bf3",
+    "url": "/static/media/robot.afd16a27.png"
+  }
+]);
